8006c8bb94d9f72895eeaf3f316627e085f4b1e1
vladimir_skrypnikov <sevgrad@list.ru>
2023-11-08 18:42:20 +0300

    Pets saving was done

--- HumanFriends ver.2/src/main/java/model/saving/SaveObjectsToDB.java
+++ HumanFriends ver.2/src/main/java/model/saving/SaveObjectsToDB.java
@@ -22,6 +22,9 @@ public class SaveObjectsToDB implements Saveable {
 
     @Override
     public String saveObjects(PetsRegistry<Pet> activeRegistry) {
+        clearTable("dogs");
+        clearTable("cats");
+        clearTable("hamsters");
         for (Pet pet: activeRegistry) {
             String type = pet.getClass().getSimpleName();
             switch (type) {
@@ -33,17 +36,31 @@ public class SaveObjectsToDB implements Saveable {
         return "Pets was saved to DB.";
     }
 
+    private void clearTable(String table) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            try (Connection con = DriverManager.getConnection(url, user, password)) {
+                String query = "DELETE FROM " + table + ";";
+                PreparedStatement prepSt = con.prepareStatement(query);
+                prepSt.executeUpdate();
+            }
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(SaveObjectsToDB.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException(ex.getMessage());
+        }
+    }
+
     private void saveToDB(String table, int typeId, Pet pet) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             try (Connection con = DriverManager.getConnection(url, user, password)) {
-                String query = "INSERT INTO ? (name, birthday, commands, animal_kind_id) VALUES (?, ?, ?, ?);";
+                String query =
+                        "INSERT INTO " + table + " (name, birthday, commands, animal_kind_id) VALUES (?, ?, ?, ?);";
                 PreparedStatement prepSt = con.prepareStatement(query);
-                prepSt.setString(1, table);
-                prepSt.setString(2, pet.getName());
-                prepSt.setDate(3, Date.valueOf(pet.getBirthdayDate()));
-                prepSt.setString(4, String.join(",", pet.getCommands()));
-                prepSt.setInt(5, typeId);
+                prepSt.setString(1, pet.getName());
+                prepSt.setDate(2, Date.valueOf(pet.getBirthdayDate()));
+                prepSt.setString(3, String.join(",", pet.getCommands()));
+                prepSt.setInt(4, typeId);
                 prepSt.executeUpdate();
             }
         } catch (ClassNotFoundException | SQLException ex) {
